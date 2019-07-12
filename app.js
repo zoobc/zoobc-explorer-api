b8e472a06565962146186a5d19476474c001df09
Eko Santoso <eksant@gmail.com>
2019-07-12 18:33:33 +0800

    [App] Fixed bug CLI start and stop (#14)
    - Refactor app.js
    - Deprecate argv.js
    - Fixed command line for binary file:
       - start: <explorer_binary_file> start
       - stop: <explorer_binary_file> stop

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-require('./main/server').start();
+const argv = process.argv[2];
+
+switch (argv) {
+  case 'start':
+    require('./main/server').start();
+    break;
+  case 'stop':
+    require('./main/server').stop();
+    break;
+  case 'port':
+    require('./main/server').port();
+    break;
+  case '--help' || '-h':
+    require('./main/help')();
+    break;
+  default:
+    require('./main/server').init();
+    break;
+}
