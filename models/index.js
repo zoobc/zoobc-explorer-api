37070a8ecb2a1a32b603b99f06771d6b751ba6df
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-07-16 10:51:43 +0800

    delete old models and init new schema

--- models/index.js
+++ models/index.js
@@ -1,11 +1,7 @@
 const Block = require('./Block');
 const Transaction = require('./Transaction');
-const AccountBalances = require('./AccountBalances');
-const Peers = require('./Peers');
 
 module.exports = {
   Block,
   Transaction,
-  AccountBalances,
-  Peers,
 };
