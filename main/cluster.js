96fcff69a47712fd8ce6b7f2689c4e41064c1975
eksant <eksant@gmail.com>
2019-07-19 00:59:58 +0800

    [Pkg] Portable app for MacOS

--- main/cluster.js
+++ main/cluster.js
@@ -1,9 +1,6 @@
 const os = require('os');
 const chalk = require('chalk');
 const cluster = require('cluster');
-const fs = require('fs');
-
-const config = require('../config/config');
 
 module.exports = (server, port, modeCluster) => {
   if (cluster.isMaster && modeCluster) {
@@ -20,10 +17,6 @@ module.exports = (server, port, modeCluster) => {
         chalk.green('🚀')
       );
     });
-
-    if (typeof process.pid !== 'undefined') {
-      fs.writeFileSync(config.app.pidPath, process.pid, { encoding: 'utf8' });
-    }
 
     process.on('SIGINT', () => {
       server.close(err => {
--- utils/kill-port.js
+++ utils/kill-port.js
@@ -1,23 +1,27 @@
-const sh = require('./shell-exec');
+const exec = require('child_process').exec;
 
-module.exports = (port, method = 'tcp') => {
+module.exports = (port, method = 'tcp', opts = {}) => {
   port = Number.parseInt(port);
+  opts = Object.assign({ cwd: process.cwd(), encoding: 'utf8' }, opts);
 
   if (!port) {
     return Promise.reject(new Error('Invalid argument provided for port'));
   }
 
+  let cmd;
   if (process.platform === 'win32') {
-    return sh(
-      `Stop-Process -Id (Get-Net${
-        method === 'UDP' ? 'UDP' : 'TCP'
-      }Connection -LocalPort ${port}).OwningProcess -Force`
-    );
+    cmd = `FOR /F "tokens=5 delims= " %P IN ('netstat -ano ^| find "LISTENING" ^| find ":${port} "') DO (TASKKILL /PID %P)`;
+  } else {
+    cmd = `lsof -i ${method === 'udp' ? 'udp' : 'tcp'}:${port} | grep ${
+      method === 'udp' ? 'UDP' : 'LISTEN'
+    } | awk '{print $2}' | xargs kill -9`;
   }
 
-  return sh(
-    `lsof -i ${method === 'udp' ? 'udp' : 'tcp'}:${port} | grep ${
-      method === 'udp' ? 'UDP' : 'LISTEN'
-    } | awk '{print $2}' | xargs kill -9`
-  );
+  exec(cmd, opts, (e, stdout, stderr) => {
+    if (e instanceof Error) {
+      throw e;
+    }
+    console.log('stdout ', stdout);
+    console.log('stderr ', stderr);
+  });
 };
