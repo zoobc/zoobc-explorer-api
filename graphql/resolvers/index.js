1b7ac263fb75a8b860fef81f86ac6eedf9c84d69
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-07-19 14:10:34 +0800

    delete all unimplemeted rest and graphql

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,4 @@
 const BlockResolvers = require('./BlockResolvers');
 const TransactionResolvers = require('./TransactionResolvers');
-const AccountBalanceResolvers = require('./AccountBalanceResolvers');
-const PeerResolvers = require('./PeerResolvers');
 
-module.exports = [BlockResolvers, TransactionResolvers, AccountBalanceResolvers, PeerResolvers];
+module.exports = [BlockResolvers, TransactionResolvers];
--- restapi/controllers/index.js
+++ restapi/controllers/index.js
@@ -1,11 +1,7 @@
 const BlockController = require('./BlockController');
 const TransactionController = require('./TransactionController');
-const AccountBalancesController = require('./AccountBalancesController');
-const PeersController = require('./PeersController');
 
 module.exports = {
   blockController: new BlockController(),
   transactionController: new TransactionController(),
-  accountBalancesController: new AccountBalancesController(),
-  peersController: new PeersController(),
 };
