7d666e51804052cae2bd0770fcba15d0a7212cf7
KevinH2810 <kevinhadisutanto@gmail.com>
2019-07-19 11:55:58 +0800

    fixing transactionsGraph names and routes

--- main/routes.js
+++ main/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   app.use(config.app.mainRoute + '/transactions', apiLimiter);
   app.use(config.app.mainRoute + '/transactions', require('../restapi/routes/transactions'));
   app.use(config.app.mainRoute + '/transactions-graph', apiLimiter);
-  app.use(config.app.mainRoute + '/transactions-graph', require('../restapi/routes/transactions-graph'));
+  app.use(config.app.mainRoute + '/transactions-graph', require('../restapi/routes/transactionsGraph'));
   app.use(config.app.mainRoute + '/account-balances', apiLimiter);
   app.use(config.app.mainRoute + '/account-balances', require('../restapi/routes/accountBalances'));
   app.use(config.app.mainRoute + '/peers', apiLimiter);
