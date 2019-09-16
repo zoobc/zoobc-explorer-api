401a9130b78fe2153ade75181af0599b387bbb47
I Gede Tirtanata <igedetirtanata@gmail.com>
2019-09-16 07:45:35 +0700

    fix tab docs api/1-introduction

--- docs/api/1-introduction.js
+++ docs/api/1-introduction.js
@@ -2,7 +2,6 @@
  * @swagger
  * tags:
  *  - name: Introduction
- *    description:
- *      <p>Welcome to the documentation for the ZooBC Explorer API!</p>
- *      <p><a href="http://en.wikipedia.org/wiki/REST_API" target="_blank">REST</a> is a web-service protocol that lends itself to rapid development by using everyday HTTP and JSON technology.</p>
+ *    description: <p>Welcome to the documentation for the ZooBC Explorer API!</p>
+ *                 <p><a href="http://en.wikipedia.org/wiki/REST_API" target="_blank">REST</a> is a web-service protocol that lends itself to rapid development by using everyday HTTP and JSON technology.</p>
  */
