f2eccb9e1c3d4e78ddfacba1c7ce27db911609a6
KevinH2810 <kevinhadisutanto@gmail.com>
2019-09-20 19:08:18 +0800

    adding s to file name resolver

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,6 @@
 const blockResolvers = require('./block.resolvers');
 const transactionResolvers = require('./transaction.resolvers');
 const accountResolvers = require('./account.resolvers');
-const searchResolvers = require('./search.resolver');
+const searchResolvers = require('./search.resolvers');
 
 module.exports = [blockResolvers, transactionResolvers, accountResolvers, searchResolvers];
