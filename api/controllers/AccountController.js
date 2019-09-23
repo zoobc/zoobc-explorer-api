f438d38e5a18bb3cb21c3316f03846c8ccd14f3b
KevinH2810 <kevinhadisutanto@gmail.com>
2019-09-23 14:32:18 +0800

    RestAPI for Node Endpoint

--- api/controllers/AccountController.js
+++ api/controllers/AccountController.js
@@ -1,4 +1,3 @@
-// const moment = require('moment');
 const BaseController = require('./BaseController');
 const HandleError = require('./HandleError');
 const { AccountsService } = require('../services');
