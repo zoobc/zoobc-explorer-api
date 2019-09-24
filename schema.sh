ec392080b7213228fdfefa181e8e47e2924c8858
eksant <eksant@gmail.com>
2019-09-24 17:26:45 +0800

    [FIX] Zoobc-Schema cannot cloned in the server

--- schema.sh
+++ schema.sh
@@ -19,6 +19,18 @@ if [ -d "./zoobc-schema" ]; then
 fi
 git clone git@github.com:zoobc/zoobc-schema.git
 
-# 2. finish
+# 2. copy repo zoobc-schema
+if [ -d "./schema" ]; then
+  rm -rf "./schema"
+fi
+mkdir -p "./schema"
+cp -R ./zoobc-schema/google ./schema/
+cp -R ./zoobc-schema/model ./schema/
+cp -R ./zoobc-schema/service ./schema/
+
+# 3. delete repo zoobc-schema
+rm -rf zoobc-schema
+
+# 4. finish
 duration=$SECONDS
 echo -e "\n$(echo_done) Done in $(($duration % 60)) seconds."
