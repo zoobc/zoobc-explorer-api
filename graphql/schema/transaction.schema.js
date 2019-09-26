fd939c8a4e7815cea195b67e9ed7736d2a2063f2
gedeyandi456 <43771081+gedeyandi456@users.noreply.github.com>
2019-09-26 13:32:43 +0800

    [Hotfix] GraphQL Query Param (#102)

--- graphql/schema/transaction.schema.js
+++ graphql/schema/transaction.schema.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 module.exports = gql`
   extend type Query {
     transactions(page: Int, limit: Int, order: String): Transactions!
-    transaction(TransactionID: ID!): Transaction!
+    transaction(TransactionID: String!): Transaction!
   }
 
   type Transactions {
