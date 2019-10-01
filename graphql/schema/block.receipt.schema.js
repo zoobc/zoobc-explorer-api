8235dbcdb916632bcad43d6d48d6a1c134d9bbd0
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-10-01 17:19:23 +0800

    add block receipt graphql

---
+++ graphql/schema/block.receipt.schema.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+  extend type Query {
+    blockReceipts(page: Int, limit: Int, order: String, BlockID: String): BlockReceipts!
+  }
+
+  type BlockReceipts {
+    BlockReceipts: [BlockReceipt!]!
+    Paginate: Paginate!
+  }
+
+  type BlockReceipt {
+    _id: ID!
+    BlockID: String
+    Height: Int
+    SenderPublicKey: String
+    ReceiverPublicKey: String
+    DataType: String
+    DataHash: String
+    ReceiptMerkleRoot: String
+    ReceiverSignature: String
+  }
+`;
