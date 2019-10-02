897881fcc854d4693a8cc6039a7f4203a159a58e
Eko Santoso <eksant@gmail.com>
2019-10-02 14:04:49 +0800

    [SCHEDULER] Adding field conversion at every field who has currency (#114)
    * [App] Refactor scheduler & update schema mongo
    
    * Update total rewards
    
    * Update schema blocks
    
    * [App] Update scheduler & schema blocks
    
    * Update item PopChange
    
    * [Scheduler] New strategy rollback & reconnecting mongodb
    
    * Add remove duplicates data blocks
    
    * [DRAFT] Work in progress new strategy get transactions
    
    * [SCHEDULER] Add block receipts
    
    * Update block receipts
    
    * [SCHEDULER] Restore schema blocks
    
    * Make cluster
    
    * [SCHEDULER] Fixing blocksmith
    
    * [SCHEDULER] Adding field conversion at every field who has currency

--- models/Accounts.js
+++ models/Accounts.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 const { upsertMany } = require('../utils');
 
-const schema = new mongoose.Schema({
-  _id: { type: String },
-  AccountAddress: { type: String },
-  Balance: { type: Number },
-  SpendableBalance: { type: Number },
-  FirstActive: { type: Date } /** ..waiting core */,
-  LastActive: { type: Date } /** ..waiting core */,
-  TotalRewards: { type: Number } /** ..waiting core */,
-  TotalFeesPaid: { type: Number } /** ..waiting core */,
-  NodePublicKey: { type: String },
-  BlockHeight: { type: Number } /** additional */,
-  PopRevenue: { type: Buffer } /** additional */,
-});
+const schema = new mongoose.Schema(
+  {
+    _id: { type: String },
+    AccountAddress: { type: String },
+    Balance: { type: Number },
+    BalanceConversion: { type: Number },
+    SpendableBalance: { type: Number },
+    SpendableBalanceConversion: { type: Number },
+    FirstActive: { type: Date } /** ..waiting core */,
+    LastActive: { type: Date } /** ..waiting core */,
+    TotalRewards: { type: Number } /** ..waiting core */,
+    TotalRewardsConversion: { type: Number },
+    TotalFeesPaid: { type: Number } /** ..waiting core */,
+    TotalFeesPaidConversion: { type: Number },
+    NodePublicKey: { type: String },
+    BlockHeight: { type: Number } /** additional */,
+    PopRevenue: { type: Buffer } /** additional */,
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
 
 schema.plugin(upsertMany);
 
--- models/Nodes.js
+++ models/Nodes.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 const { upsertMany } = require('../utils');
 
-const schema = new mongoose.Schema({
-  _id: { type: String },
-  NodePublicKey: { type: String },
-  OwnerAddress: { type: String } /** AccountAddress */,
-  NodeAddress: { type: String },
-  LockedFunds: { type: String } /** LockedBalance */,
-  RegisteredBlockHeight: { type: Number } /** RegistrationHeight */,
-  ParticipationScore: { type: Number } /** ..waiting core */,
-  RegistryStatus: { type: Boolean } /** Queued */,
-  BlocksFunds: { type: Number } /** ..waiting core */,
-  RewardsPaid: { type: Number } /** ..waiting core */,
-  Latest: { type: Boolean } /** additional */,
-  Height: { type: Number } /** additional */,
-  NodeID: { type: String } /** additional */,
-});
+const schema = new mongoose.Schema(
+  {
+    _id: { type: String },
+    NodePublicKey: { type: String },
+    OwnerAddress: { type: String } /** AccountAddress */,
+    NodeAddress: { type: String },
+    LockedFunds: { type: String } /** LockedBalance */,
+    RegisteredBlockHeight: { type: Number } /** RegistrationHeight */,
+    ParticipationScore: { type: Number } /** ..waiting core */,
+    RegistryStatus: { type: Boolean } /** Queued */,
+    BlocksFunds: { type: Number } /** ..waiting core */,
+    RewardsPaid: { type: Number } /** ..waiting core */,
+    Latest: { type: Boolean } /** additional */,
+    Height: { type: Number } /** additional */,
+    NodeID: { type: String } /** additional */,
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
 
 schema.plugin(upsertMany);
 
