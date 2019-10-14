c6ab0d3375e1bcea15d6478c7cefa64ef4b32bf1
gedenata <igedetirtanata@gmail.com>
2019-10-14 15:17:53 +0800

    [Fix] Explorer API CircleCi

---
+++ .circleci/deploy.sh
@@ -0,0 +1,11 @@
+set -e	# set -e
+rm -rf .git	# rm -rf .git
+rm -rf .gitignore	# rm -rf .gitignore
+git config --global user.email "igedetirtanata@gmail.com"	# git config --global user.email "igedetirtanata@gmail.com"
+git config --global user.name "gedenata"	# git config --global user.name "gedenata"
+
+
+pm2 stop zoobcexplorer:6969	# pm2 stop zoobcexplorer:6969
+git pull origin develop	# git pull origin develop
+rm -rf node_modules/ yarn.lock && yarn	# rm -rf node_modules/ yarn.lock && yarn
+pm2 start zoobcexplorer:6969
