2c4f3b16b194f7c128e154b1fc9afd234276dfb0
KevinH2810 <kevinhadisutanto@gmail.com>
2019-10-23 16:07:34 +0800

    after newest pull

--- models/index.js
+++ models/index.js
@@ -2,13 +2,8 @@ const Nodes = require('./Nodes');
 const Blocks = require('./Blocks');
 const Accounts = require('./Accounts');
 const Transactions = require('./Transactions');
-<<<<<<< HEAD
-const PublishedReceipts = require('./PublishedReceipts');
-const BatchReceipt = require('./BatchReceipt');
-=======
 const BatchReceipt = require('./BatchReceipt');
 const PublishedReceipts = require('./PublishedReceipts');
->>>>>>> de1082b26fadcd57b7d9551550c70c14fd0ae2d3
 const AccountTransactions = require('./AccountTransactions');
 
 module.exports = {
@@ -16,12 +11,7 @@ module.exports = {
   Blocks,
   Accounts,
   Transactions,
-<<<<<<< HEAD
-  PublishedReceipts,
-  BatchReceipt,
-=======
   BatchReceipt,
   PublishedReceipts,
->>>>>>> de1082b26fadcd57b7d9551550c70c14fd0ae2d3
   AccountTransactions,
 };
