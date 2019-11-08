0214b9083037ac6058b8a4cb020697808fc17b49
eksant <eksant@gmail.com>
2019-11-08 14:45:23 +0800

    [App] Update schema blocks and add model user account

--- api/services/index.js
+++ api/services/index.js
@@ -1,4 +1,5 @@
 const BaseService = require('./BaseService');
+const UsersService = require('./UsersService');
 const NodesService = require('./NodesService');
 const ResetsService = require('./ResetsService');
 const BlocksService = require('./BlocksService');
@@ -9,6 +10,7 @@ const AccountTransactionsService = require('./AccountTransactionsService');
 
 module.exports = {
   BaseService,
+  UsersService,
   NodesService,
   ResetsService,
   BlocksService,
