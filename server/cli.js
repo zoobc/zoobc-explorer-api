f858fc187ba13ddda433447aa331712d44d0dc23
KevinH2810 <kevinhadisutanto@gmail.com>
2019-11-22 10:57:22 +0800

    adding login and reset

--- server/cli.js
+++ server/cli.js
@@ -3,34 +3,57 @@ let command = Value[0];
 let Username = '';
 let Password = '';
 
-if (Value.length != 5) {
-  console.log('need more parameter');
-  return;
-}
-
 switch (command) {
   case 'zoobc':
-    if (Value[1] === '-u') {
-      Username = Value[2];
-    } else if (Value[1] === '-p') {
-      Password = Value[2];
+    if (Value[1] == 'login') {
+      if (Value.length != 6) {
+        console.log('need more parameter');
+        break;
+      }
+
+      if (Value[2] === '-u') {
+        Username = Value[4];
+      } else if (Value[2] === '-p') {
+        Password = Value[4];
+      } else {
+        console.log(Value[2], 'is invalid command');
+        break;
+      }
+
+      if (Value[4] === '-u') {
+        Username = Value[5];
+      } else if (Value[4] === '-p') {
+        Password = Value[5];
+      } else {
+        console.log(Value[4], 'is invalid command');
+        break;
+      }
+
+      let Hash = (
+        Math.random()
+          .toString(36)
+          .substring(2, 16) +
+        Math.random()
+          .toString(36)
+          .substring(2, 16)
+      ).toUpperCase();
+      console.log(Hash);
+      break;
+    } else if (Value[1] == 'reset') {
+      if (Value.length != 3) {
+        console.log('need more parameter');
+        break;
+      }
+
+      //CROSS CHECK THE INPUTED HASH WITH THE LATEST HASH IN DB
+
+      console.log('DB RESETED');
+      break;
     } else {
-      console.log(Value[1], 'is invalid command');
+      console.log('Invalid Command, please use -h for help');
       break;
     }
 
-    if (Value[3] === '-u') {
-      Username = Value[4];
-    } else if (Value[3] === '-p') {
-      Password = Value[4];
-    } else {
-      console.log(Value[3], 'is invalid command');
-      break;
-    }
-
-    console.log('Username = ', Username);
-    console.log('Password = ', Password);
-    break;
   default:
     console.log('invalid input');
 }
