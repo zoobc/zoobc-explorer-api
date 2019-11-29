8305602684e201f7d37ce17d0b1e534c9462ecd8
gedenata <igedetirtanata@gmail.com>
2019-11-29 11:24:07 +0800

    test ssh circleci

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pm2 stop zoobcexplorer:6969
-git pull origin develop
-rm -rf node_modules/ yarn.lock && yarn
-pm2 start zoobcexplorer:6969
+# pm2 stop zoobcexplorer:6969
+# git pull origin develop
+# rm -rf node_modules/ yarn.lock && yarn
+# pm2 start zoobcexplorer:6969
+
+pm2 deploy ecosystem.config.js testnet update
