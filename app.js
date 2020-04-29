18b89ba1d3a870c513027fc7491846446b43b010
kevinh2810 <kevinhadisutanto@gmail.com>
2020-04-29 16:53:23 +0700

    stopping scheduler from running

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ const express = require('express');
 const config = require('./config/config');
 const app = express().set('port', config.app.port);
 
-const server =
-  !fs.existsSync(config.app.openSslKeyPath) || !fs.existsSync(config.app.openSslCertPath)
-    ? http.createServer(app)
-    : https.createServer(
-        {
-          key: fs.readFileSync(config.app.openSslKeyPath),
-          cert: fs.readFileSync(config.app.openSslCertPath),
-        },
-        app
-      );
+const server = !fs.existsSync(config.app.openSslKeyPath) || !fs.existsSync(config.app.openSslCertPath) ?
+  http.createServer(app) :
+  https.createServer({
+      key: fs.readFileSync(config.app.openSslKeyPath),
+      cert: fs.readFileSync(config.app.openSslCertPath),
+    },
+    app
+  );
 
 require('./modules/cors')(app);
 require('./modules/compression')(app);
@@ -26,6 +24,6 @@ require('./modules/cluster')(server);
 require('./modules/redis')();
 require('./modules/mongoose')();
 require('./api/routes')(app);
-require('./scheduler').start();
+require('./scheduler').stop();
 
 module.exports = app;
