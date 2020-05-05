a6898ac491b5a64da54e7356bed98d3e683a0406
Kevin Hadi <47102992+KevinH2810@users.noreply.github.com>
2020-05-05 09:35:39 +0700

    Stopping scheduler from running (#178)
    * stopping scheduler from running
    * adding extra line

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const express = require('express');
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+const express = require("express");
 
-const config = require('./config/config');
-const app = express().set('port', config.app.port);
+const config = require("./config/config");
+const app = express().set("port", config.app.port);
 
 const server =
-  !fs.existsSync(config.app.openSslKeyPath) || !fs.existsSync(config.app.openSslCertPath)
+  !fs.existsSync(config.app.openSslKeyPath) ||
+  !fs.existsSync(config.app.openSslCertPath)
     ? http.createServer(app)
     : https.createServer(
         {
@@ -17,15 +18,15 @@ const server =
         app
       );
 
-require('./modules/cors')(app);
-require('./modules/compression')(app);
-require('./modules/log')(app);
-require('./modules/swagger')(app);
-require('./modules/graphql')(app, server);
-require('./modules/cluster')(server);
-require('./modules/redis')();
-require('./modules/mongoose')();
-require('./api/routes')(app);
-require('./scheduler').start();
+require("./modules/cors")(app);
+require("./modules/compression")(app);
+require("./modules/log")(app);
+require("./modules/swagger")(app);
+require("./modules/graphql")(app, server);
+require("./modules/cluster")(server);
+require("./modules/redis")();
+require("./modules/mongoose")();
+require("./api/routes")(app);
+require("./scheduler").stop();
 
 module.exports = app;
