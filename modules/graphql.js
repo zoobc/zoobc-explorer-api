79a052e4478847a62f79a32462dfb1614a09a81a
eksant <eksant@gmail.com>
2020-07-22 21:01:57 +0800

    Testing subscription

--- modules/graphql.js
+++ modules/graphql.js
@@ -1,4 +1,5 @@
-const { ApolloServer } = require('apollo-server-express')
+// const { ApolloServer } = require('apollo-server-express')
+const { ApolloServer } = require('apollo-server')
 
 const models = require('../models')
 const resolvers = require('../graphql/resolvers')
@@ -9,19 +10,34 @@ const config = require('../config/config')
 
 module.exports = (app, server) => {
   const apolloServer = new ApolloServer({
-    introspection: true,
     typeDefs,
     resolvers,
+    tracing: true,
     context: { models },
     subscriptions: {
       path: `${config.app.mainRoute}/graphql`,
       onConnect: () => console.log('Connected to websocket'),
-      onDisconnect: () => console.log('Disconnected from websocket'),
     },
   })
 
-  apolloServer.applyMiddleware({ app, path: `${config.app.mainRoute}/graphql` })
-  msg.green('🚀', `http://${config.app.host}:${config.app.port}${apolloServer.graphqlPath}`)
-  apolloServer.installSubscriptionHandlers(server)
-  msg.green('🚀', `ws://${config.app.host}:${config.app.port}${apolloServer.subscriptionsPath}`)
+  apolloServer.listen(config.app.port).then(({ url, subscriptionsUrl }) => {
+    console.log('==url', url)
+    console.log('==subscriptionsUrl', subscriptionsUrl)
+  })
+
+  // const apolloServer = new ApolloServer({
+  //   introspection: true,
+  //   typeDefs,
+  //   resolvers,
+  //   context: { models },
+  //   subscriptions: {
+  //     path: `${config.app.mainRoute}/graphql`,
+  //     onConnect: () => console.log('Connected to websocket'),
+  //     onDisconnect: () => console.log('Disconnected from websocket'),
+  //   },
+  // })
+  // apolloServer.applyMiddleware({ app, path: `${config.app.mainRoute}/graphql` })
+  // msg.green('🚀', `http://${config.app.host}:${config.app.port}${apolloServer.graphqlPath}`)
+  // apolloServer.installSubscriptionHandlers(server)
+  // msg.green('🚀', `ws://${config.app.host}:${config.app.port}${apolloServer.subscriptionsPath}`)
 }
